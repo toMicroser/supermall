88e3864af8f2e585736562cc71bc58794a34a0e8
toMicroser <974897765@qq.com>
2022-09-09 20:38:01 +0800

    初始化项目

---
+++ src/components/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home" activeColor="green">
+      <img slot="item-icon" src="~assets/img/tabbar/1.svg" alt="" />
+      <img slot="item-icon-active" src="../../assets/img/tabbar/5.svg" alt="" />
+      <div slot="item-text">首页</div>
+    </tab-bar-item>
+    <tab-bar-item path="/category">
+      <img slot="item-icon" src="~assets/img/tabbar/2.svg" alt="" />
+      <img slot="item-icon-active" src="../../assets/img/tabbar/6.svg" alt="" />
+      <div slot="item-text">分类</div>
+    </tab-bar-item>
+    <tab-bar-item path="/cart">
+      <img slot="item-icon" src="~assets/img/tabbar/3.svg" alt="" />
+      <img slot="item-icon-active" src="~assets/img/tabbar/7.svg" alt="" />
+      <div slot="item-text">购物</div>
+    </tab-bar-item>
+    <tab-bar-item path="/profile">
+      <img slot="item-icon" src="~assets/img/tabbar/4.svg" alt="" />
+      <img slot="item-icon-active" src="~assets/img/tabbar/8.svg" alt="" />
+      <div slot="item-text">我的</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+import TabBar from "../../components/common/tabbar/TabBar";
+import TabBarItem from "../../components/commom/tabbar/TabBarItem";
+export default {
+  name: "MainTabBar",
+  components: {
+    TabBar,
+    TabBarItem,
+  },
+};
+</script>
+
+<style scoped>
+</style>
